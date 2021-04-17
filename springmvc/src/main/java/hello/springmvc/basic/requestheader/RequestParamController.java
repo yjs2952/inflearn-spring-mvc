6ed8a00112a4e896e019394ab0358a9f6dfa922b
yjs2952 <yjs2952@naver.com>
2021-04-17 21:25:34 +0900

    HTTP 요청 파라미터 - 쿼리 파라미터, HTML Form

---
+++ springmvc/src/main/java/hello/springmvc/basic/requestheader/RequestParamController.java
@@ -0,0 +1,26 @@
+package hello.springmvc.basic.requestheader;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@RestController
+public class RequestParamController {
+
+    /**
+     * 반환 타입이 없으면서 이렇게 응답에 값을 직접 집어넣으면, view 조회X
+     */
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+        log.info("username={}, age={}", username, age);
+
+        response.getWriter().write("ok");
+    }
+}
